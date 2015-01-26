98802338704eb2dd30a1046dd8ad38e54c0dabb1
Ernest Cunningham <git@ging.co.nz>
2015-01-26 15:19:04 +1300

    Initial Commit

---
+++ Audio Recording and Playback/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Audio Recording and Playback
+//
+//  Created by Ernest Cunningham on 26/01/15.
+//  Copyright (c) 2015 Icy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Audio Recording and Playback/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Audio Recording and Playback
+//
+//  Created by Ernest Cunningham on 26/01/15.
+//  Copyright (c) 2015 Icy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
